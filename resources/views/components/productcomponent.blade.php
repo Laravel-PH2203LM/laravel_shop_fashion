5e63026ec7a9540dfb48aca8366303abb0976875
dinhsan <dinhsan200@gmail.com>
2022-09-21 13:18:44 +0700

    them component trong product-detail
    Signed-off-by: dinhsan <dinhsan200@gmail.com>

---
+++ resources/views/components/productcomponent.blade.php
@@ -0,0 +1,112 @@
+<!--product section area start-->
+<section class="product_section related_product">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section_title">
+                    <h2>Related Products</h2>
+                    <p>Contemporary, minimal and modern designs embody the Lavish Alice handwriting</p>
+                </div>
+            </div>
+        </div>
+        <div class="product_area">
+            <div class="row">
+                <div class="product_carousel product_three_column4 owl-carousel">
+                    <div class="col-lg-3">
+                        <div class="single_product">
+                            <div class="product_thumb">
+                                <a class="primary_img" href="{{url('/shop/product')}}"><img src="{{asset('fontend/img/product/product21.jpg')}}" alt=""></a>
+                                <a class="secondary_img" href="{{url('/shop/product')}}"><img src="{{asset('fontend/img/product/product22.jpg')}}" alt=""></a>
+                                <div class="product_action">
+                                    <div class="hover_action">
+                                        <a  href="#"><i class="fa fa-plus"></i></a>
+                                        <div class="action_button">
+                                            <ul>
+
+                                                <li><a title="add to cart" href="{{url('/cart')}}"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                                                <li><a href="{{url('/wishlist')}}" title="Add to Wishlist"><i class="fa fa-heart-o" aria-hidden="true"></i></a></li>
+
+                                                <li><a href="{{url('/compare')}}" title="Add to Compare"><i class="fa fa-sliders" aria-hidden="true"></i></a></li>
+
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="quick_button">
+                                    <a href="#" data-toggle="modal" data-target="#modal_box" title="quick_view">+ quick view</a>
+                                </div>
+
+                                <div class="product_sale">
+                                    <span>-7%</span>
+                                </div>
+                            </div>
+                            <div class="product_content">
+                                <h3><a href="{{url('/shop/product')}}">Marshall Portable  Bluetooth</a></h3>
+                                <span class="current_price">£60.00</span>
+                                <span class="old_price">£86.00</span>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
+<!--product section area end-->
+
+<!--product section area start-->
+<section class="product_section upsell_product">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section_title">
+                    <h2>Upsell Products</h2>
+                    <p>Contemporary, minimal and modern designs embody the Lavish Alice handwriting</p>
+                </div>
+            </div>
+        </div>
+        <div class="product_area">
+            <div class="row">
+                <div class="product_carousel product_three_column4 owl-carousel">
+                    <div class="col-lg-3">
+                        <div class="single_product">
+                            <div class="product_thumb">
+                                <a class="primary_img" href="{{url('/shop/product')}}"><img src="{{asset('fontend/img/product/product15.jpg')}}" alt=""></a>
+                                <a class="secondary_img" href="{{url('/shop/product')}}"><img src="{{asset('fontend/img/product/product16.jpg')}}" alt=""></a>
+                                <div class="product_action">
+                                    <div class="hover_action">
+                                        <a  href="#"><i class="fa fa-plus"></i></a>
+                                        <div class="action_button">
+                                            <ul>
+
+                                                <li><a title="add to cart" href="{{url('/cart')}}"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                                                <li><a href="{{url('/wishlist')}}" title="Add to Wishlist"><i class="fa fa-heart-o" aria-hidden="true"></i></a></li>
+
+                                                <li><a href="{{url('/compare')}}" title="Add to Compare"><i class="fa fa-sliders" aria-hidden="true"></i></a></li>
+
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="quick_button">
+                                    <a href="#" data-toggle="modal" data-target="#modal_box" title="quick_view">+ quick view</a>
+                                </div>
+
+                                <div class="product_sale">
+                                    <span>-7%</span>
+                                </div>
+                            </div>
+                            <div class="product_content">
+                                <h3><a href="{{url('/shop/product')}}">Marshall Portable  Bluetooth</a></h3>
+                                <span class="current_price">£60.00</span>
+                                <span class="old_price">£86.00</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
